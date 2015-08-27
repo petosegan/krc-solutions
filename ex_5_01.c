86a3192be66079460e4ad4ce02cfbea3dce01953
petosegan <rwturner17@gmail.com>
2015-08-27 19:36:23 -0400

    Ex 5 01: Push signed nondigits back to input
    Ex 5 01 asks me to amend the 'getint' function so that '-' or '+'
    followed by a nondigit is no longer treated as a valid representation of
    zero, and is instead pushed back onto the input. The effect is that the
    appearance of an input like '-a' will now halt the scan, filling the
    rest of the target array with zeroes. I don't really understand why this
    is desirable behavior though.

---
+++ ex_5_01.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define SIZE 100
+
+int getch(void);
+void ungetch(int);
+
+/* getint: get next integer from input into *pn*/
+int getint(int *pn)
+{
+	int c, sign, signchar;
+
+	while (isspace(c = getch())) /* skip white space */
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+		ungetch(c); /* it's not a number */
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-') {
+		signchar = c;
+		if (!isdigit(c = getchar())) {
+			ungetch(c);
+			return 0;
+		}
+		ungetch(c);
+	}
+
+	for (*pn = 0; isdigit(c); c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
+
+int main()
+{
+	int n, array[SIZE];
+	for (n = 0; n < SIZE; n++)
+		array[n] = 0;
+	for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+		;
+	for (n = 0; n < SIZE; n++)
+		printf("%d ", array[n]);
+	printf("\n\n");
+}
+
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
