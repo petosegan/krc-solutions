3ba790379ed9e668259fb479a512ea285ac48a85
petosegan <rwturner17@gmail.com>
2015-08-28 18:00:12 -0400

    Handle leap years via pointers
    I rewrote day_of_year and month_day to only use pointer arithmetic,
    including for handling the leap years. This was a bit harder than I
    thought. I initially wrote 'q = daytab + leap' but this is incorrect,
    since I actually want q to point to the integers in the appropriate
    array, not the array itself. 'q = *(daytab) + leap' is also wrong, it
    just gives off by one errors for leap years. 'q = *(daytab + leap)'
    makes q point to the first element of the correct array.

--- ex_5_09.c
+++ ex_5_09.c
@@ -16,12 +16,12 @@ int day_of_year(int year, int month, int day)
 	}
 
 	leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
-	q = daytab[leap];
-	if (day <= 0 || day > *(daytab[leap] + month)) {
+	q = *(daytab + leap);
+	if (day <= 0 || day > *((*daytab + leap) + month)) {
 		printf("\nInvalid Day");
 		return -1;
 	}
-	while ((q - daytab[leap]) < month)
+	while ((q - *(daytab + leap)) < month)
 		day += *(q++);
 	return day;
 }
@@ -32,14 +32,14 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
 	char *q;
 
 	leap = (year%4 == 0 && year%100 != 0) || year%400 == 0;
-	q = daytab[leap];
+	q = *(daytab + leap);
 
 	if (yearday <= 0 || yearday > (365 + leap)){
 		printf("\nInvalid Day of Year");
 	}
 	while (yearday > *q)
 		yearday -= *(q++);
-	*pmonth = q - daytab[leap];
+	*pmonth = q - *(daytab + leap);
 	*pday = yearday;
 }
 
@@ -50,8 +50,14 @@ int main()
 	int day;
 
 	printf("\n%d should be 1", day_of_year(2014, 1, 1));
+	printf("\n%d should be 60", day_of_year(2014, 3, 1));
+	printf("\n%d should be 61", day_of_year(2016, 3, 1));
 	month_day(2014, 1, &month, &day);
 	printf("\n%d, %d should be 1, 1", month, day);
+	month_day(2014, 60, &month, &day);
+	printf("\n%d, %d should be 3, 1", month, day);
+	month_day(2016, 60, &month, &day);
+	printf("\n%d, %d should be 2, 29", month, day);
 	day_of_year(2014, 13, 1);
 	day_of_year(2014, 1, 44);
 	month_day(2014, 400, &month, &day);
