3dc106f64844b24964e42b16e87a5420869c2e2d
petosegan <rwturner17@gmail.com>
2015-08-29 23:56:25 -0400

    Ex 5 14: Add reverse option to sortlines
    Ex 5 14 asks me to 'modify the sort program to handle a -r flag, which
    indicates sorting in reverse (decreasing) order.' Initially I thought
    the most straightforward thing was to reverse the array of strings after
    sorting. However, I ran into trouble with the pointer arithmetic, and
    saw strange things like truncated lines. I believe that the issue is
    that when using alloc, the sizes of the elements are not predictable so
    the pointer arithmetic is not accurate.
    
    I then abandoned this approach and instead rewrote qsort slightly to
    reverse the comparison operator. This should work for any comparison
    function that conforms to the standard of strmp.

---
+++ ex_5_14.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <string.h>
+
+#define ALLOCSIZE 10000
+#define MAXLINES 5000
+#define MAXLEN 1000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void my_qsort(void *lineptr[], int left, int right, int rev,
+		int (*comp)(void *, void *));
+int numcmp(char *, char *);
+void reverse(char *lineptr[], int nlines);
+
+/* sort input lines */
+int main(int argc, char *argv[])
+{
+	int nlines;
+	int numeric = 0;
+	int rev = 1;
+	char c;
+
+	while (--argc > 0 && (*++argv)[0] == '-') { 
+		while ((c = *++argv[0])) {
+			switch(c) {
+				case 'n':
+					numeric = 1;
+					break;
+				case 'r':
+					rev = -1;
+					break;
+				default:
+					printf("sortlines: illegal option %c\n", c);
+					argc = -1;
+					break;
+			}
+		}
+	}
+	if (argc != 0)
+		printf("Usage: sortlines -n -r");
+	else {
+
+		if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+			my_qsort((void **) lineptr, 0, nlines-1, rev,
+					(int (*)(void*,void*))(numeric ? numcmp : strcmp));
+			writelines(lineptr, nlines);
+			return 0;
+		} else {
+			printf("input too big to sort\n");
+			return 1;
+		}
+	}
+}
+
+void reverse(char *lineptr[], int nlines)
+{
+	char temp[MAXLEN];
+	char **end = lineptr + nlines;
+
+	while (lineptr < end) {
+		strcpy(temp, *lineptr);
+		strcpy(*(lineptr++), *end);
+		strcpy(*(end--), temp);
+	}
+}
+
+
+void my_qsort(void *v[], int left, int right, int rev,
+		int (*comp)(void *, void *))
+{
+	int i, last;
+	void swap(void *v[], int, int);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+	for (i = left+1; i <= right; i++) {
+		if ((*comp)(v[i], v[left]) * rev < 0)
+			swap(v, ++last, i);
+	}
+	swap(v, left, last);
+	my_qsort(v, left, last-1, rev, comp);
+	my_qsort(v, last+1, right, rev, comp);
+}
+
+#include <stdlib.h>
+
+int numcmp(char *s1, char *s2)
+{
+	double v1, v2;
+
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if (v1 < v2)
+		return -1;
+	else if (v1 > v2)
+		return 1;
+	else
+		return 0;
+}
+
+void swap(void *v[], int i, int j)
+{
+	void *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
+
+int my_getline(char *, int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines)
+{
+	int len, nlines;
+	char *p, line[MAXLEN];
+
+	nlines = 0;
+	while ((len=my_getline(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || (p = alloc(len)) == NULL)
+			return -1;
+		else {
+			line[len-1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+		}
+	}
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+	int i;
+
+	for (i=0; i<nlines; i++)
+		printf("%s\n", lineptr[i]);
+}
+
+int my_getline(char *p, int lim)
+{
+	int c;
+	char *q;
+	q = p;
+
+	while((c=getchar()) != EOF && c != '\n')
+	{
+		*(q++) = c;
+	}
+	if (c == '\n') {
+		*(q++) = c;
+	}
+	*q = '\0';
+	return (q-p);
+}
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n)
+{
+	if (allocbuf + ALLOCSIZE - allocp >= n) {
+		allocp += n;
+		return allocp - n;
+	}
+	else
+		return 0;
+}
+
+void afree(char *p)
+{
+	if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+		allocp = p;
+}
