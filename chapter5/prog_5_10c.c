d00099f2c7822ccea371df9db9d3a425c8bb218c
petosegan <rwturner17@gmail.com>
2015-09-02 10:57:37 -0400

    Consolidate chapter 5 problems

---
+++ chapter5/prog_5_10c.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int my_getline(char *line, int max);
+
+/* find: print lines that match pattern from 1st arg */
+int main(int argc, char *argv[])
+{
+	char line[MAXLINE];
+	long lineno = 0;
+	int c, except = 0, number = 0, found = 0;
+
+	while (--argc > 0 && (*++argv)[0] == '-')
+		while ((c = *++argv[0])) {
+			switch(c) {
+				case 'x':
+					except = 1;
+					break;
+				case 'n':
+					number = 1;
+					break;
+				default:
+					printf("find: illegal option %c\n", c);
+					argc = 0;
+					found = -1;
+					break;
+			}
+		}
+	if (argc != 1)
+		printf("Usage: find -x -n pattern\n");
+	else
+		while (my_getline(line, MAXLINE) > 0) {
+			lineno++;
+			if ((strstr(line, *argv) != NULL) != except) {
+				if (number)
+					printf("%ld:", lineno);
+				printf("%s", line);
+				found++;
+			}
+		}
+	return found;
+}
+
+int my_getline(char *p, int lim)
+{
+	int c;
+	char *q;
+	q = p;
+
+	while((c=getchar()) != EOF && c != '\n')
+	{
+		*(q++) = c;
+	}
+	if (c == '\n') {
+		*(q++) = c;
+	}
+	*q = '\0';
+	return (q-p);
+}
